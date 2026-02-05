331ad5cdf751fa8634b0d4d93bcf5a840b07027f
Mariano18014 <hasanmariano5@gmail.com>
2026-02-05 14:24:33 -0300

    Modifico carpetas del proyecto y instalo la biblioteca react-router-dom.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
+  document.getElementById('root')
 )
