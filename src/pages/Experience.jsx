56f43ae70ea1d205ca74b358a940db328c121193
Mariano18014 <hasanmariano5@gmail.com>
2026-02-06 17:08:19 -0300

    Agrego clase Experience en la carpeta pages.

---
+++ src/pages/Experience.jsx
@@ -0,0 +1,21 @@
+function Experience() {
+    return (
+        <section id="experience">
+            <h2>Experiencia</h2>
+            <article>
+                <h3>Desarrollador Flutter</h3>
+                <span>Hospital de Tigre | Marzo 2025 - Julio 2025</span>
+
+                <ul>
+                    <li>Análisis funcional y relevamiento de requerimientos junto al equipo.</li>
+                    <li>Definición del MVP y planificación del desarrollo de la aplicación móvil.</li>
+                    <li>Desarrollo de la aplicación utilizando Flutter y Dart.</li>
+                    <li>Diseño de interfaces y prototipos en Figma.</li>
+                    <li>Trabajo colaborativo bajo metodología Scrum, utilizando GitHub y Firebase.</li>
+                </ul>
+            </article>
+        </section>
+    )
+}
+
+export default Experience
