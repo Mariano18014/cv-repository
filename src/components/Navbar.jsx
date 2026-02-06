e75ff9206755ee87d974d5aa4767952f69724d81
Mariano18014 <hasanmariano5@gmail.com>
2026-02-06 13:03:43 -0300

    Agrego el componete Navbar y modifico el archivo de rutas App.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar () {
+    return (
+        <nav>
+            <a href="#inicio">Inicio</a>
+            <a href="#sobre-mi">Sobre mi</a>
+            <a href="#experiencia">Experiencia</a>
+            <a href="#tecnologia">Tecnologias</a>
+            <a href="#contacto">Contacto / Redes</a>
+        </nav>
+    )
+}
+
+export default Navbar
