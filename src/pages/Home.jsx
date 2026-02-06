7833d0995fc25623a1ba05861e9753368064f412
Mariano18014 <hasanmariano5@gmail.com>
2026-02-06 13:12:24 -0300

    Agrego la clase Home en la sección de pages.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+function Home() {
+    return (
+        <section id="inicio">
+            <h1>Mariano Hasan</h1>
+            <h2>Analista de Sistemas</h2>
+            <a>
+                Desarrollador junior enfocado en desarrollo web y mobile,
+                con interés en crecer profesionalmente dentro del mundo IT.
+            </a>
+
+            <a id="experiencia">
+                Ver experiencia  
+            </a>
+        </section>
+    )
+}
+
+export default Home;
