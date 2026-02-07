c51f1d10ed022332b46a3445e947a2855aa41129
Mariano18014 <hasanmariano5@gmail.com>
2026-02-07 15:14:32 -0300

    Agrego clase ContactNetwork a la carpeta pages.

---
+++ src/pages/ContactNetworks.jsx
@@ -0,0 +1,36 @@
+function ContactNetworks() {
+    return (
+        <section id="contact-networks">
+            <h2>Contacto / Redes</h2>
+             <ul>
+                <li>
+                    <a
+                        href="https://www.linkedin.com/in/mariano-hasan"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        LinkedIn
+                    </a>
+                </li>
+
+                <li>
+                    <a
+                        href="https://github.com/Mariano18014"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        GitHub
+                    </a>
+                </li>
+
+                <li>
+                    <a href="mailto:hasanmariano27@gmail.com">
+                        hasanmariano27@gmail.com
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )   
+}
+
+export default ContactNetworks
