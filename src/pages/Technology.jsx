b880c475cac5c2579aaf50e6003578211881f331
Mariano18014 <hasanmariano5@gmail.com>
2026-02-07 14:05:58 -0300

    Agregue la clase Technology en la carpeta pages.

---
+++ src/pages/Technology.jsx
@@ -0,0 +1,64 @@
+function Technology() {
+    return (
+        <section id="technology">
+            <h2>Tecnologías</h2>
+            <div>
+                <h3>Lenguajes</h3>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>Dart</li>
+                    <li>Java</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3>Frameworks y runtimes</h3>
+                <ul>
+                    <li>React (JSX)</li>
+                    <li>Flutter</li>
+                    <li>Node.js</li>
+                    <li>Express</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3>Conceptos y arquitectura</h3>
+                <ul>
+                    <li>Principios SOLID</li>
+                    <li>APIs REST / RESTful</li>
+                    <li>SPA</li>
+                    <li>UML</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3>Herramientas</h3>
+                <ul>
+                    <li>GitHub</li>
+                    <li>Postman</li>
+                    <li>Jira</li>
+                    <li>Visual Studio Code</li>
+                    <li>Figma</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3>Base de datos</h3>
+                <ul>
+                    <li>SQL Server</li>
+                    <li>Firebase</li>
+                    <li>Mongo DB</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3>Metodologías</h3>
+                <ul>
+                    <li>Scrum</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Technology
