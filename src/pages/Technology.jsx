6724db77d3f75508e594d9de6e5b8ac8f0339fdb
Mariano18014 <hasanmariano5@gmail.com>
2026-02-11 10:35:06 -0300

    Agrego estilos al archivo Technology.

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -1,3 +1,5 @@
+import '../styles/Technology.css'
+
 function Technology() {
     return (
         <section id="technology">
@@ -7,19 +9,19 @@ function Technology() {
                 <ul>
                     <li>
                         <img
-                        source="![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)"
+                        src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"
                         alt="JavaScript"
                         />
                     </li>
                     <li>
                         <img
-                        source="![Dart](https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&logo=dart&logoColor=white)"
+                        src="https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&logo=dart&logoColor=white"
                         alt="Dart"
                         />
                     </li>
                     <li>
                         <img
-                        source="![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white)"
+                        src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white"
                         alt="Java"
                         />
                     </li>
@@ -31,25 +33,25 @@ function Technology() {
                 <ul>
                     <li>
                         <img
-                        source="![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)"
+                        src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
                         alt="React (JSX)"
                         />
                     </li>
                     <li>
                         <img
-                        source="![Flutter](https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white)"
+                        src="https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white"
                         alt="Flutter"
                         />
                     </li>
                     <li>
                         <img
-                        source="![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)"
+                        src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white"
                         alt="Node.js"
                         />
                     </li>
                     <li>
                         <img
-                        source="![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB)"
+                        src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB"
                         alt="Express.js"
                         />
                     </li>
@@ -71,31 +73,31 @@ function Technology() {
                 <ul>
                     <li>
                         <img
-                        source="![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)"
+                        src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white"
                         alt="GitHub"
                         />
                     </li>
                     <li>
                         <img
-                        source="![Postman](https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white)"
+                        src="https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white"
                         alt="Postman"
                         />
                     </li>
                     <li>
                         <img
-                        source="![Jira](https://img.shields.io/badge/jira-%230A0FFF.svg?style=for-the-badge&logo=jira&logoColor=white)"
+                        src="https://img.shields.io/badge/jira-%230A0FFF.svg?style=for-the-badge&logo=jira&logoColor=white"
                         alt="Jira"
                         />
                     </li>
                     <li>
                         <img
-                        source="![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)"
+                        src="https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white"
                         alt="Visual Studio Code"
                         />
                     </li>
                     <li>
                         <img
-                        source="![Figma](https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white)"
+                        src="https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white"
                         alt="Figma"
                         />
                     </li>
@@ -105,16 +107,21 @@ function Technology() {
             <div>
                 <h3>Base de datos</h3>
                 <ul>
-                    <li>SQL Server</li>
                     <li>
                         <img
-                        source="![Firebase](https://img.shields.io/badge/firebase-a08021?style=for-the-badge&logo=firebase&logoColor=ffcd34)"
+                        src="https://img.shields.io/badge/Microsoft_SQL_Server-CC2927?style=for-the-badge&logo=microsoftsqlserver&logoColor=white"
+                        alt="SQL Server"
+                        />
+                    </li>
+                    <li>
+                        <img
+                        src="https://img.shields.io/badge/firebase-a08021?style=for-the-badge&logo=firebase&logoColor=ffcd34"
                         alt="Firebase"
                         />
                     </li>
                     <li>
                         <img
-                        source="![MongoDB](https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white)"
+                        src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white"
                         alt="MongoDB"
                         />
                     </li>
@@ -124,12 +131,7 @@ function Technology() {
             <div>
                 <h3>Metodologías</h3>
                 <ul>
-                    <li>
-                        <img
-                        source="https://img.shields.io/badge/Microsoft_SQL_Server-CC2927?style=for-the-badge&logo=microsoftsqlserver&logoColor=white"
-                        alt="SQL Server"
-                        />
-                    </li>
+                    <li>Scrum</li>
                 </ul>
             </div>
         </section>
